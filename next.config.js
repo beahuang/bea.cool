a4add95aff507021a26f87e1f45b86d8db22cb55
Beatrice Huang <beahuang1@gmail.com>
2022-01-15 12:51:01 -0500

    Add in basic styling architecture and imports

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
-}
+  /**
+   * Pass custom node-sass options.
+   */
+  sassOptions: {
+    /**
+     * Include `styles/abstracts` directory in Sass paths.
+     *
+     * @see https://github.com/sass/node-sass/#includepaths
+     */
+    includePaths: [path.join(__dirname, 'styles/abstracts')],
+
+    /**
+     * Automatically import abstracts into stylesheets.
+     *
+     * @see https://webpack.js.org/loaders/sass-loader/#additionaldata
+     */
+    additionalData: "@import 'styles/abstracts';",
+  },
+};
