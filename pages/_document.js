0e005aef7db35c850ae6505d3aa20575f4132ef3
Beatrice Huang <beahuang1@gmail.com>
2022-01-18 11:55:49 -0500

    Add apple touch icon

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" key="favicon" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
