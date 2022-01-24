cc51b7d17c15bcb525496f582ec2c1e8fd26128b
Beatrice Huang <beahuang1@gmail.com>
2022-01-24 13:21:47 -0500

    Add in google tag manager configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,47 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../utils/gtag';
+import Script from 'next/script';
 
 import { Layout, Header } from 'components';
 import 'styles/globals.scss';
 
+const handleRouteChange = url => {
+  gtag.pageview(url);
+};
+
 export default function App({ Component, pageProps }) {
   const [view, setView] = useState('GALLERY');
 
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Layout>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Header view={view} setView={setView} />
       <Component {...pageProps} view={view} />
     </Layout>
