e2fa897956ec154ab70d204388cbacd8860aebdf
Beatrice Huang <beahuang1@gmail.com>
2022-01-26 23:03:51 -0500

    Makes our title and description more seo friendly

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,7 +4,10 @@ import { Grid, SiteMeta } from 'components';
 export default function Layout({ children }) {
   return (
     <>
-      <SiteMeta title="ʕ•́ᴥ•̀ʔっ bea" description="~ engineer ~" />
+      <SiteMeta
+        title="ʕ•́ᴥ•̀ʔっ bea huang | engineer"
+        description="a portfolio to showcase the engineering skills of beatrice huang"
+      />
       <Grid />
       <main>{children}</main>
     </>
